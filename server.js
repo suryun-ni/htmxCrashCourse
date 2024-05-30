8d85a2c94b1993b9f060129ddd07903c628b56b6
suryun-ni <tengkusurya29@gmail.com>
2024-05-30 13:20:23 +0700

    add hx-indicator

--- server.js
+++ server.js
@@ -15,16 +15,22 @@ app.listen(3000, () => {
 });
 
 //Handle GET request to fetch users
-app.get("/users", (req, res) => {
-  const users = [
-    { id: 1, name: "John Doe" },
-    { id: 2, name: "Bob Williams" },
-    { id: 3, name: "Shannon Jackson" },
-  ];
-  res.send(`
+app.get("/users", async (req, res) => {
+  // const users = [
+  //   { id: 1, name: "John Doe" },
+  //   { id: 2, name: "Bob Williams" },
+  //   { id: 3, name: "Shannon Jackson" },
+  // ];
+
+  //loading 2 second
+  setTimeout(async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    res.send(`
   <h1 class="text-2xl font-bold my-4"> Users</h1>
   <ul>
   ${users.map((user) => `<li>${user.name}</li>`).join("")}
   </ul>
   `);
+  }, 2000);
 });
