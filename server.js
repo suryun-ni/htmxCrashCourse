8a16327f69753e30125e6cbeb3e074b70001f93e
suryun-ni <tengkusurya29@gmail.com>
2024-05-30 22:32:55 +0700

    add hx-vals

--- server.js
+++ server.js
@@ -24,7 +24,10 @@ app.get("/users", async (req, res) => {
 
   //loading 2 second
   setTimeout(async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const limit = +req.query.limit || 10;
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users?_limit=${limit}`
+    );
     const users = await response.json();
     res.send(`
   <h1 class="text-2xl font-bold my-4"> Users</h1>
@@ -32,5 +35,5 @@ app.get("/users", async (req, res) => {
   ${users.map((user) => `<li>${user.name}</li>`).join("")}
   </ul>
   `);
-  }, 2000);
+  }, 1000);
 });
